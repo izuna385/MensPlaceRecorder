9eb1cff21e12926dd2578583dd3a7505cdfb88db
izuna385 <izuna385@gmail.com>
2021-02-27 04:15:49 +0000

    validation をpostに追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  # belongs_to :category
+  validates :title,  presence: true,  length: { in: 1..75 } 
+  validates :category_id, presence: true
   validates :description, presence: true, length: { maximum: 255 }
   mount_uploader :image, ImageUploader
 end
